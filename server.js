c5e6eca6f4e7a8daa40b5eb17b44a92a04bd52f7
Scott <scott.zetterstrom@gmail.com>
2023-01-13 18:31:59 -0500

    hello world express

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const app = express();
+
+const port = 5000;
+
+app.get('/', (req, res)=>{
+    res.send('Hello World')
+})
+
+app.listen(port, ()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+})
