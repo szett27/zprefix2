c58b36f47665b0435b3bfa5c0b42c9ed5462f5e4
Scott <scott.zetterstrom@gmail.com>
2023-01-13 20:31:10 -0500

    express now serves basic react app

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res)=>{
-    res.send('Hello World')
-})
+app.use(express.static(path.join(__dirname, 'frontend', 'build')))
+
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html')
+)});
 
 app.listen(port, ()=>{
     console.log(`Example app listening at http://localhost:${port}`)
