d893d89114914b860d3d1817af24db5e4680b596
Scott <scott.zetterstrom@gmail.com>
2023-01-21 12:15:00 -0500

    working on inventory

--- frontend/src/Inventory.js
+++ frontend/src/Inventory.js
@@ -1,27 +1,47 @@
 import {useState} from 'react';
 import { Button } from '@mui/material';
+import {Card} from '@mui/material'
+import Item from './item'
 
 export default function Inventory(props){
 //display all inventory, paginate?
 //sample inventory
 const [inventory, setInventory] =useState(0);
 const [item, setItem] = useState(false);
-const [newItem, setNewItem] = useState(false)
+const [newItem, setNewItem] = useState(false);
+const [ InventoryLoaded, setInventoryLoaded] = useState('false')
 
-// function componentDidMount(){
-//   fetch('/items')
-//   .then((response) => response.json())
-//   .then((data) => setInventory(data));
-// }
+function componentDidMount(){
+  fetch('http://localhost:5000/inventory')
+  .then((response) => response.json())
+  .then((data) => setInventory(data))
+  .then(console.log(inventory))
+  .then(()=>setInventoryLoaded(true));
+}
 
-return(
+
+if(setInventory){
+    return(
+
   <div>
   <Button onClick = {()=>setInventory(0)}>Inventory</Button>
-    {/* {item ? <Item id={key} item = {item} setItem = {setItem} auth ={props.auth} user ={props.userID}/>
-     : inventory.map((item, key)=>{
+    {item ? <Item item = {item} setItem = {setItem} auth ={props.auth} user ={props.user}/> :
+     props.myInventory ? inventory.filter(item=>(item.id===props.user)).map((item, key)=>{
+        return(
+            <Item id={key} item = {item} setItem = {setItem} auth ={props.auth} user ={props.userID}/>
+        )
+     }) : inventory.map((item, key)=>{
     <Item id={key} item = {item} setItem = {setItem} auth ={props.auth} user ={props.userID}/>
-  })} */}
+  })}
   </div>
+) } 
+else{
+
+return (
+    <div>
+        <p>Invenotry Loading...</p>
+    </div>
 )
+}
 
 }
---
+++ frontend/src/item.js
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import {Button, Card, CardContent, Typography, Stack, Switch} from '@mui/material'
+
+
+export default function Item(props){
+
+
+    const [item_name, setItem_Name] = useState(props.item.name)
+    const [description, setDescription] = useState(props.item.description)
+    const [quantity, setQuantity] = useState(props.item.quantity)
+    const [item_id, setItem_id] = useState(props.item.id)
+    const [edit, setEdit] = useState(false)
+
+    return(
+       <div>
+        <Card>
+        <CardContent>
+        {props.auth && props.user === props.item.user_id ?<Stack direction="row" spacing={1} alignItems="center"> <Typography></Typography><Switch defaultChecked inputProps={{ 'aria-label': 'ant design' }} />
+        <Typography>Edit</Typography></Stack> : <></>}
+      <Typography sx={{ fontSize: 14 }} editable = {edit} color="text.secondary" gutterBottom>
+        {item_name}
+      </Typography>
+      <Typography sx={{ fontSize: 14 }} editable = {edit} color="text.secondary" gutterBottom>
+        {quantity}
+      </Typography>
+      <Typography sx={{ fontSize: 14 }} editable = {edit}  color="text.secondary" gutterBottom>
+        {description}
+      </Typography>
+      {props.auth && props.user === props.item.user_id ? <Button >Delete</Button>: <></>}
+        </CardContent>
+        </Card>
+       </div>
+
+    )
+}
