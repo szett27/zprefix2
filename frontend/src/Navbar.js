f70743a2b7ed142bb7e7e93033a800455157ce9f
Scott <scott.zetterstrom@gmail.com>
2023-01-23 21:49:58 -0500

    fixed logout

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -10,6 +10,12 @@ import Button from '@mui/material/Button';
 
 
 export default function Navbar(props) {
+  function logout(e){
+    e.preventDefault(e)
+    props.setLoginStatus(true);
+    props.auth(false);
+  
+  }
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -17,7 +23,7 @@ export default function Navbar(props) {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>Inventory Management</Typography>
         {props.auth ?  <><Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>Hello {props.user}</Typography>
         <Button color ="inherit" onClick={()=>props.setMyInventory(true)}>My Inventory</Button><Button color = "inherit" onClick={()=>props.setCreateItem(true)}>Add Item</Button><Button color = "inherit" onClick={()=>props.setAuth(false)}>Logout</Button></>: <></>}
-        {!props.auth ?<><Button color = "inherit" type = "submit" onClick={()=>props.setLoginStatus(true)}>Login</Button></> :<></>}
+        {!props.auth ?<><Button color = "inherit" type = "submit" onClick={(e)=>logout(e)}>Login</Button></> :<></>}
         </Toolbar>
       </AppBar>
     </Box>
